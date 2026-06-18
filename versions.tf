7b5b5deee9e8b6add97ff64543f15737bc281d12
Mohan Lakshmanan <thefossgeek@gmail.com>
2026-06-18 22:58:47 +0400

    chore: add versions.tf file

---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.76.0, < 5.0.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "3.8.0"
+    }
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">= 1.15.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.11.0"
+    }
+  }
+}
